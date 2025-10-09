52d4e55e5438e70799b6f31f40bd683b24b8cd1c
Wilfried OLLIVIER <wollivier@fdn.fr>
2025-10-09 15:18:00 +0200

    feat: add cachable resolving logic to cachable_response

--- internal/cache/cachable_response.go
+++ internal/cache/cachable_response.go
@@ -3,6 +3,7 @@ package cache
 import (
 	"bytes"
 	"net/http"
+	"time"
 )
 
 type CachableResponse struct {
@@ -29,6 +30,54 @@ func (cr *CachableResponse) WriteHeader(statusCode int) {
 	cr.ResponseWriter.WriteHeader(statusCode)
 }
 
-func IsCachableRequest(r *http.Request) bool {
-	return r.Method == http.MethodGet
+var cachableResponseStatuses = []int{
+	http.StatusOK,                   // 200
+	http.StatusNonAuthoritativeInfo, // 203
+	http.StatusNoContent,            // 204
+	http.StatusPartialContent,       // 206
+	http.StatusMultipleChoices,      // 300
+	http.StatusMovedPermanently,     // 301
+	http.StatusNotFound,             // 404
+	http.StatusMethodNotAllowed,     // 405
+	http.StatusGone,                 // 410
+	http.StatusRequestURITooLong,    // 414
+	http.StatusNotImplemented,       // 501
+}
+
+func isCachableStatus(status int) bool {
+	for _, s := range cachableResponseStatuses {
+		if status == s {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (cr *CachableResponse) CacheTTL() (bool, time.Duration) {
+	present, duration := ParseCacheControl(cr.Header().Get("Cache-Control")).TTL()
+	if present {
+		return true, duration
+	}
+
+	expiresHeader := cr.Header().Get("Expires")
+	if expiresHeader != "" {
+		if expiresTime, err := http.ParseTime(expiresHeader); err == nil {
+			return true, time.Until(expiresTime)
+		}
+	}
+
+	return false, 0
+}
+
+func (cr *CachableResponse) IsCachable() bool {
+	return isCachableStatus(cr.StatusCode) && ParseCacheControl(cr.Header().Get("Cache-Control")).isCachable()
+}
+
+func (cr *CachableResponse) IsCachableConsideringAuth() bool {
+	return isCachableStatus(cr.StatusCode) && ParseCacheControl(cr.Header().Get("Cache-Control")).isCachable()
+}
+
+func IsRequestCachable(requestMethod string) bool {
+	return requestMethod == http.MethodGet
 }
